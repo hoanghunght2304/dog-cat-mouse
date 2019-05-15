98d194515d84b237946af067d158ecddd5f229e4
HoangHung <hoanghunght2304@gmail.com>
2019-05-15 22:51:33 +0700

    Let dog eat cat

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const Dog = require('./dog');
 const Cat = require('./cat');
+const Mouse = require('./mouse');
 
 const dog = new Dog('Tom');
 const tom = new Cat();
+const mouse = new Mouse();
 dog.eat(tom);
+console.log(dog);
+
